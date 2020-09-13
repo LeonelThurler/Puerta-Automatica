22f70e010473b3d6694d64708a41235ea5f0518f
Franco Leonel Thurler <70031921+LeonelThurler@users.noreply.github.com>
2020-09-13 11:37:57 -0300

    Update Trabajo practico.cpp

--- Trabajo practico.cpp
+++ Trabajo practico.cpp
@@ -39,7 +39,7 @@ while(h=!0){
 				sensoraf=0;
 				puerta_automatica(sensorad,sensorad,cierre);
 			}
-			while((hora->tm_hour)<horainicio || (hora->tm_hour)>horacierre ){
+			while((hora->tm_hour)<horainicio || (hora->tm_hour)>=horacierre ){
 				cierre=1;
 				sensoraf=0;
 				printf("Hay personas adentro queriendo salir?\n");
