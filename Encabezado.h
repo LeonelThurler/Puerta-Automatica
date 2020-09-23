a77325bf0bd25ec678a7acb28ffe5cfbab2f21c6
Franco Leonel Thurler <70031921+LeonelThurler@users.noreply.github.com>
2020-09-23 14:36:04 -0300

    Add files via upload

---
+++ Encabezado.h
@@ -0,0 +1,30 @@
+#ifndef Encabezado
+#define Encabezado
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int  horainicio,horacierre,sensorad,sensoraf,cierre,boton,opcion,tiempo,l,o;
+FILE *ptr;
+
+typedef enum {
+   apertura =1,
+   cerrado = 2 ,
+   panico = 3 ,
+   carga =4
+} posiblesestados_t;
+
+int emergencia(int boton);
+
+int horarioapertura(void);
+
+int horariocierre(void);
+
+void puerta_automatica (int sensorad, int sensoraf,int cierre);
+
+int cargahorarios(void);
+
+int caso(void);
+
+
+#endif
